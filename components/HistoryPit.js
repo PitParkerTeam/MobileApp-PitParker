689b26a93cdf30e51bd9bc8fb9b5e90f7cbd1cd6
Xiaoben Yin <70068293+XiaobenYin@users.noreply.github.com>
2022-11-23 16:33:47 -0800

    added basic components

---
+++ components/HistoryPit.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function HistoryPit() {
+  return (
+    <View>
+      <Image />
+      <Text>Location Info</Text>
+      <Text>Parking time</Text>
+      <Text>Price</Text>
+      <Text>Duration</Text>
+    </View>
+  )
+}
