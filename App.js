a887614ebd739c731da80c60e8f3585f28be52e2
littlerzrz <renxinyi.design@gmail.com>
2022-11-29 19:22:54 -0800

    bottom tabs finished

--- App.js
+++ App.js
@@ -1,40 +1,48 @@
-import { StatusBar } from "expo-status-bar";
+import { useCallback } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { HistoryDetails, PitDetails, SignIn, Main, AddNewParking } from "./screens";
+import {
+  HistoryDetails,
+  PitDetails,
+  SignIn,
+  Main,
+  AddNewParking,
+} from "./screens";
 import getUser from "./firebase/firestore";
 
 const Stack = createNativeStackNavigator();
 const options = { headerShown: false };
 
+
 export default function App() {
+
   return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Main" component={Main} options={options} />
-          <Stack.Screen
-            name="HistoryDetails"
-            component={HistoryDetails}
-            options={options}
-          />
-          <Stack.Screen
-            name="PitDetails"
-            component={PitDetails}
-            options={options}
-          />
-          <Stack.Screen
-            name="SignIn"
-            component={SignIn}
-            options={{ headerTitle: "Sign in" }}
-          />
-          <Stack.Screen
-            name="AddNewParking"
-            component={AddNewParking}
-            options={{ headerTitle: "Add New Parking" }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={Main} options={options} />
+        <Stack.Screen
+          name="HistoryDetails"
+          component={HistoryDetails}
+          options={options}
+        />
+        <Stack.Screen
+          name="PitDetails"
+          component={PitDetails}
+          options={options}
+        />
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{ headerTitle: "Sign in" }}
+        />
+        <Stack.Screen
+          name="AddNewParking"
+          component={AddNewParking}
+          options={{ headerTitle: "Add New Parking" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- components/BottomTab.js
+++ components/BottomTab.js
@@ -1,59 +1,108 @@
 import { View, Text, TouchableOpacity, SafeAreaView } from "react-native";
 import React from "react";
 import PitButton from "./PitButton";
+import { COLORS } from "../common";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+const icons = {
+  Home:'home-outline',
+  Favorites:'star-circle-outline',
+  History:'history',
+  Account:'account-circle-outline'
+}
+
+const AddTab = ({ navigation }) => {
+  return (
+    <PitButton
+      onPress={() => navigation.navigate("AddNewParking")}
+      style={{ width: 50 }}
+      type="primary"
+      text="+"
+    ></PitButton>
+  );
+};
+const Tab = (props) => {
+  const { route, index, descriptors, navigation, state } = props;
+  if (route.name == "Add") return <AddTab {...props} />;
+  const { options } = descriptors[route.key];
+  const label = options.tabBarLabel
+    ? options.tabBarLabel
+    : options.title
+    ? options.title
+    : route.name;
+
+  const isFocused = state.index === route.idx;
+
+  const onPress = () => {
+    const event = navigation.emit({
+      type: "tabPress",
+      target: route.key,
+      canPreventDefault: true,
+    });
+
+    if (!isFocused && !event.defaultPrevented) {
+      // The `merge: true` option makes sure that the params inside the tab screen are preserved
+      navigation.navigate({ name: route.name, merge: true });
+    }
+  };
+
+  const tabColor = isFocused ? COLORS.TINT[100] : COLORS.BASE[60]
+  return (
+    <TouchableOpacity
+      accessibilityRole="button"
+      accessibilityState={isFocused ? { selected: true } : {}}
+      accessibilityLabel={options.tabBarAccessibilityLabel}
+      testID={options.tabBarTestID}
+      onPress={onPress}
+      style={{ alignItems: "center" }}
+    >
+      <Icon name={icons[route.name]} size={24} color={tabColor} />
+      <Text
+        style={{
+          color: tabColor,
+        }}
+      >
+        {label}
+      </Text>
+    </TouchableOpacity>
+  );
+};
 
 export default function BottomTab({ state, descriptors, navigation }) {
+  const { routes } = state;
+  const [home, saved, history, account] = routes.map((route, idx) => ({
+    ...route,
+    idx,
+  }));
+  const add = {
+    name: "Add",
+    key: "add",
+    params: undefined,
+    idx: 5,
+  };
+  const newRoutes = [home, saved, add, history, account];
   return (
     <SafeAreaView>
-      <View style={{ flexDirection: "row" }}>
-        {state.routes.map((route, index) => {
-          const { options } = descriptors[route.key];
-          const label =
-            options.tabBarLabel !== undefined
-              ? options.tabBarLabel
-              : options.title !== undefined
-              ? options.title
-              : route.name;
-
-          const isFocused = state.index === index;
-
-          const onPress = () => {
-            const event = navigation.emit({
-              type: "tabPress",
-              target: route.key,
-              canPreventDefault: true,
-            });
-
-            if (!isFocused && !event.defaultPrevented) {
-              // The `merge: true` option makes sure that the params inside the tab screen are preserved
-              navigation.navigate({ name: route.name, merge: true });
-            }
-          };
-
-          const onLongPress = () => {
-            navigation.emit({
-              type: "tabLongPress",
-              target: route.key,
-            });
-          };
-
-          return (
-            <TouchableOpacity
-              accessibilityRole="button"
-              accessibilityState={isFocused ? { selected: true } : {}}
-              accessibilityLabel={options.tabBarAccessibilityLabel}
-              testID={options.tabBarTestID}
-              onPress={onPress}
-              onLongPress={onLongPress}
-              style={{ flex: 1 }}
-              key={route.key}
-            >
-              <Text style={{ color: isFocused ? "#673ab7" : "#222" }}>
-                {label}
-              </Text>
-            </TouchableOpacity>
-          );
-        })}
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding:15,
+          paddingTop:0,
+          borderTopColor:COLORS.BASE[40],
+          borderTopWidth:1,
+        }}
+      >
+        {newRoutes.map((route) => (
+          <Tab
+            descriptors={descriptors}
+            route={route}
+            navigation={navigation}
+            state={state}
+            key={route.key}
+          />
+        ))}
       </View>
     </SafeAreaView>
   );
