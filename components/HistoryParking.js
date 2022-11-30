8de66742f32d20bc80ae4c6e51ee02c8b5ee9634
littlerzrz <renxinyi.design@gmail.com>
2022-11-30 13:41:26 -0800

    updates on history parking name

--- components/HistoryParking.js
+++ components/HistoryParking.js
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function HistoryPit() {
+export default function HistoryParking() {
   return (
     <View>
       <Image />
