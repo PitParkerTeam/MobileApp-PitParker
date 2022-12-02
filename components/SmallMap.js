bcb4c74d26d3a7d1e4908bd20b9ebf719c7db86b
wuchenru <chenruwu31@gmail.com>
2022-12-01 22:26:16 -0800

    updates on map and pits

--- components/SmallMap.js
+++ components/SmallMap.js
@@ -1,15 +1,15 @@
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import { StyleSheet, View, Text, Dimensions, Button } from "react-native";
 import { COLORS, MAP_STYLE } from "../common";
 
-export default function SmallMap({ location, isFlat }) {
+export default function SmallMap({ location, isFlat, style }) {
   const coords = {
     latitude: location?.latitude || 37.78825,
     longitude: location?.longitude || -122.4324,
   };
   return (
     <MapView
-      style={[styles.map, isFlat ? styles.mapFlat : ""]}
+      style={[styles.map, isFlat ? styles.mapFlat : "", style]}
       region={{ ...coords, latitudeDelta: 0.01, longitudeDelta: 0.01 }}
       provider={PROVIDER_GOOGLE}
       customMapStyle={MAP_STYLE}
@@ -21,11 +21,12 @@ export default function SmallMap({ location, isFlat }) {
 
 const styles = StyleSheet.create({
   map: {
-    height: "25%",
+    width: "100%",
+    height: Dimensions.get("window").height * 0.2,
     borderRadius: 8,
   },
   mapFlat: {
-    borderBottomLeftRadius:0,
-    borderBottomRightRadius:0,
-  }
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+  },
 });
