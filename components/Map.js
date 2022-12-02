7de50f075f5d860b4db101151103a9d6fd0f5490
wuchenru <chenruwu31@gmail.com>
2022-12-01 17:32:06 -0800

    updates on maps

--- components/Map.js
+++ components/Map.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from "react";
-import MapView, { Circle, Marker, Polygon } from "react-native-maps";
+import MapView from "react-native-maps";
 import { StyleSheet, View, Text, Dimensions, Button } from "react-native";
 import * as Location from "expo-location";
-import Icon from "react-native-vector-icons/FontAwesome";
+import { COLORS } from "../common";
 
 export default function Map() {
+  const latitudeDelta = 0.01;
+  const longitudeDelta = 0.01;
+
   const [userLocation, setUserLocation] = useState({
     latitude: 37.78825,
     longitude: -122.4324,
+    latitudeDelta,
+    longitudeDelta,
   });
+
   const locateUser = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
     if (status !== "granted") {
@@ -18,33 +24,30 @@ export default function Map() {
       accuracy: Location.Accuracy.Balanced,
       enableHighAccuracy: true,
     });
-    setUserLocation(location.coords);
-  }
+    const { longitude, latitude } = location.coords;
+    setUserLocation({ longitude, latitude, latitudeDelta, longitudeDelta });
+  };
 
-
-  useEffect(() => {
+  useEffect(()=> {
     locateUser();
-  }, []);
+  },[])
 
   const mapPressed = (event) => {
-    // setCurrentLocation({
+    // setUserLocation({
     //   latitude: event.nativeEvent.coordinate.latitude,
     //   longitude: event.nativeEvent.coordinate.longitude,
     // });
   };
 
-  const { longitude, latitude } = userLocation;
-  const latitudeDelta = 0.01;
-  const longitudeDelta = 0.01;
-  const region = { longitude, latitude, latitudeDelta, longitudeDelta };
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
         onPress={mapPressed}
         showsUserLocation
-        region={region}
+        region={userLocation}
         followsUserLocation={true}
+        // tintColor={COLORS.TINT[100]}
       ></MapView>
     </View>
   );
@@ -59,4 +62,3 @@ const styles = StyleSheet.create({
     height: Dimensions.get("window").height,
   },
 });
-
