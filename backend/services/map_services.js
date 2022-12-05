5a0920910cbc07b52c698072ba518e35c5d99bf5
littlerzrz <renxinyi.design@gmail.com>
2022-12-05 00:33:41 -0800

    added places API & updated readme

--- backend/services/map_services.js
+++ backend/services/map_services.js
@@ -1,1 +1,13 @@
 import axios from "axios";
+import { mapApiKey } from "@env";
+const baseUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch";
+
+export const getNearbyParking = async (lat, lng) => {
+  const config = {
+    method: "get",
+    url: `${baseUrl}/json?location=${lat},${lng}&radius=1000&type=parking&key=${mapApiKey}`,
+    headers: {},
+  };
+  const res = await axios(config);
+  return res;
+};
