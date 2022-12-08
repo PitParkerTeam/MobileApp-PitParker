290d04b6924d476be2924335a67b4d05fa1b520c
littlerzrz <renxinyi.design@gmail.com>
2022-12-08 15:28:35 -0800

    updated tabs on home

--- components/maps/Map.js
+++ components/maps/Map.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-import { StyleSheet, View, Dimensions } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 import * as Location from "expo-location";
-import { COLORS, MAP_STYLE } from "../../common";
-import { PitButton } from "../basics";
+import { MAP_STYLE } from "../../common";
+
 export default function Map() {
   const latitudeDelta = 0.01;
   const longitudeDelta = 0.01;
@@ -12,7 +12,6 @@ export default function Map() {
     latitude: 37.78825,
     longitude: -122.4324,
   });
-  const [showAll, setShowAll] = useState(true);
 
   const locateUser = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
@@ -38,74 +37,21 @@ export default function Map() {
     // });
   };
 
-  const TabSet = () => (
-    <View style={styles.buttons}>
-      <PitButton
-        style={[styles.button, styles.leftButton]}
-        onPress={() => setShowAll(true)}
-        text="Nearby Pits"
-        type={showAll ? "primary" : "normal"}
-      />
-      <PitButton
-        style={[styles.button, styles.rightButton]}
-        onPress={() => setShowAll(false)}
-        text="My Pits"
-        type={!showAll ? "primary" : "normal"}
-      />
-    </View>
-  );
-
   return (
-    <View style={styles.container}>
-      <TabSet/>
-      <MapView
-        style={styles.map}
-        onPress={mapPressed}
-        showsUserLocation
-        region={{ ...userLocation, ...delta }}
-        followsUserLocation={true}
-        provider={PROVIDER_GOOGLE}
-        customMapStyle={MAP_STYLE}
-        // tintColor={COLORS.TINT[100]}
-      ></MapView>
-    </View>
+    <MapView
+      style={styles.map}
+      onPress={mapPressed}
+      showsUserLocation
+      region={{ ...userLocation, ...delta }}
+      followsUserLocation={true}
+      provider={PROVIDER_GOOGLE}
+      customMapStyle={MAP_STYLE}
+      // tintColor={COLORS.TINT[100]}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-  },
-  buttons: {
-    position: "absolute",
-    zIndex: 2000,
-    top: 60,
-    flexDirection: "row",
-    backgroundColor: COLORS.BASE[0],
-    borderRadius:8
-  },
-  activeButton: {
-    backgroundColor: COLORS.TINT[100],
-    color: COLORS.BASE[0],
-  },
-  leftButton: {
-    borderBottomLeftRadius: 8,
-    borderTopLeftRadius: 8,
-    marginRight: -0.5,
-  },
-  rightButton: {
-    borderBottomRightRadius: 8,
-    borderTopRightRadius: 8,
-    marginLeft: -0.5,
-  },
-  button: {
-    margin: 0,
-    marginLeft: 0,
-    marginRight: 0,
-    borderRadius: 0,
-    width:150
-  },
   map: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -1,11 +1,66 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import {Map} from "../../components";
+import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Map, PitButton } from "../../components";
+import { COLORS } from "../../common";
 
 export default function Home() {
-  return (
-    <View>
-      <Map/>
+  const [showAll, setShowAll] = useState(true);
+  const TabSet = () => (
+    <View style={styles.buttons}>
+      <PitButton
+        style={[styles.button, styles.leftButton]}
+        onPress={() => setShowAll(true)}
+        text="Nearby Pits"
+        type={showAll ? "primary" : "normal"}
+      />
+      <PitButton
+        style={[styles.button, styles.rightButton]}
+        onPress={() => setShowAll(false)}
+        text="My Pits"
+        type={!showAll ? "primary" : "normal"}
+      />
     </View>
-  )
+  );
+  return (
+    <View style={styles.container}>
+      <TabSet />
+      <Map />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  buttons: {
+    position: "absolute",
+    zIndex: 2000,
+    top: 60,
+    flexDirection: "row",
+    backgroundColor: COLORS.BASE[0],
+    borderRadius: 8,
+  },
+  activeButton: {
+    backgroundColor: COLORS.TINT[100],
+    color: COLORS.BASE[0],
+  },
+  leftButton: {
+    borderBottomLeftRadius: 8,
+    borderTopLeftRadius: 8,
+    marginRight: -0.5,
+  },
+  rightButton: {
+    borderBottomRightRadius: 8,
+    borderTopRightRadius: 8,
+    marginLeft: -0.5,
+  },
+  button: {
+    margin: 0,
+    marginLeft: 0,
+    marginRight: 0,
+    borderRadius: 0,
+    width: 150,
+  },
+});
