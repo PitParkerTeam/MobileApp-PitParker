6f83d884794702d66d0ff146532d306178e7bb0f
Xiaoben Yin <70068293+XiaobenYin@users.noreply.github.com>
2022-12-10 00:27:35 -0800

    changed logout text to a button

--- screens/Tabs/Account.js
+++ screens/Tabs/Account.js
@@ -2,13 +2,20 @@ import { SafeAreaView, Text, Button } from "react-native";
 import React from "react";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../../api";
+import { BottomContainer, PitButton, SmallMap } from "../../components";
 
 export default function ProfileSettings() {
   return (
     <SafeAreaView>
-      <Text>
+      <BottomContainer>
+        <PitButton 
+          text="Sign Out"
+          onPress={() => signOut(auth)}
+        />
+      </BottomContainer>
+      {/* <Text>
         <Button title="Logout" onPress={() => signOut(auth)} />
-      </Text>
+      </Text> */}
     </SafeAreaView>
   );
 }
