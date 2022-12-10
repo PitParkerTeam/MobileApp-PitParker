525c98986f5d421dc47855bd00619b9a3d84bd17
littlerzrz <renxinyi.design@gmail.com>
2022-12-10 03:23:04 -0800

    added function for timestamp formatting

--- common/index.js
+++ common/index.js
@@ -1,4 +1,4 @@
 export { COLORS, DEFAULT_VARS } from "./vars";
 export { TEXT_STYLES } from "./shared_styles";
 export { MAP_STYLE } from "./map_style";
-export { formatTime, timeDiff } from "./utils";
+export { formatTime, timeDiff, formatTimestamp } from "./utils";
