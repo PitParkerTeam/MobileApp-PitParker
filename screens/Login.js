6810833d2292a2d3ac193da032e8b5aff870b844
Xiaoben Yin <70068293+XiaobenYin@users.noreply.github.com>
2022-12-10 20:03:07 -0800

    added logo to login and signup page

--- screens/Login.js
+++ screens/Login.js
@@ -1,7 +1,19 @@
-import { View, Text, TextInput, StyleSheet, Button, Alert } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Button,
+  Alert,
+  Image,
+  SafeAreaView,
+} from "react-native";
 import React, { useState } from "react";
 import { auth } from "../api";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { COLORS, TEXT_STYLES } from "../common";
+
+
 
 export default function Login({ navigation }) {
   const [email, setEmail] = useState(null);
@@ -15,37 +27,47 @@ export default function Login({ navigation }) {
     }
   };
   return (
-    <View style={styles.authContent}>
-      <Text style={styles.label}>Email Address</Text>
-      <TextInput
-        placeholder="Email"
-        style={styles.input}
-        onChangeText={(newEmail) => setEmail(newEmail)}
-        value={email}
-        keyboardType="email-address"
-      />
-      <Text style={styles.label}>password</Text>
-      <TextInput
-        style={styles.input}
-        secureTextEntry={true}
-        onChangeText={(newPass) => setPassword(newPass)}
-        value={password}
-        placeholder="Password"
-      />
-      <View style={styles.button}>
-        <Button title="Log In" onPress={handleLogin} />
-      </View>
-      <View style={styles.button}>
-        <Button
-          title="New User? Create an account"
-          onPress={() => navigation.replace("Signup")}
+    <SafeAreaView style={styles.container}>
+      <View style={styles.authContent}>
+        <View>
+          <Image source={require("./My_Location.png")} />
+        </View>
+        <Text style={styles.label}>Email Address</Text>
+        <TextInput
+          placeholder="Email"
+          style={styles.input}
+          onChangeText={(newEmail) => setEmail(newEmail)}
+          value={email}
+          keyboardType="email-address"
         />
+        <Text style={styles.label}>password</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          onChangeText={(newPass) => setPassword(newPass)}
+          value={password}
+          placeholder="Password"
+        />
+        <View style={styles.button}>
+          <Button title="Log In" onPress={handleLogin} />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title="New User? Create an account"
+            onPress={() => navigation.replace("Signup")}
+          />
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.BASE[0],
+
+  },
   authContent: {
     padding: 16,
     flex: 1,
--- screens/Signup.js
+++ screens/Signup.js
@@ -1,7 +1,19 @@
-import { View, Text, TextInput, StyleSheet, Button, Alert } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Button,
+  Alert,
+  Image,
+  SafeAreaView,
+} from "react-native";
 import React, { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, userAPI } from "../api";
+import { COLORS, TEXT_STYLES } from "../common";
+
+
 
 export default function Signup({ navigation }) {
   const [email, setEmail] = useState(null);
@@ -29,41 +41,48 @@ export default function Signup({ navigation }) {
   };
 
   return (
-    <View style={styles.authContent}>
-      <Text style={styles.label}>Email Address</Text>
-      <TextInput
-        placeholder="Email"
-        style={styles.input}
-        onChangeText={(newEmail) => setEmail(newEmail)}
-        value={email}
-        keyboardType="email-address"
-      />
-      <Text style={styles.label}>password</Text>
-      <TextInput
-        style={styles.input}
-        secureTextEntry={true}
-        onChangeText={(newPass) => setPassword(newPass)}
-        value={password}
-        placeholder="Password"
-      />
-      <Text style={styles.label}>Confirm password</Text>
-      <TextInput
-        style={styles.input}
-        secureTextEntry={true}
-        onChangeText={(newPass) => setConfirmPassword(newPass)}
-        value={confirmpassword}
-        placeholder="Password"
-      />
-      <Button title="Register" onPress={handleSignup} />
-      <Button
-        title="Already Registered? Login"
-        onPress={() => navigation.replace("Login")}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.authContent}>
+        <Image source={require("./My_Location.png")} />
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          placeholder="Email"
+          style={styles.input}
+          onChangeText={(newEmail) => setEmail(newEmail)}
+          value={email}
+          keyboardType="email-address"
+        />
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          onChangeText={(newPass) => setPassword(newPass)}
+          value={password}
+          placeholder="Password"
+        />
+        <Text style={styles.label}>Confirm password</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          onChangeText={(newPass) => setConfirmPassword(newPass)}
+          value={confirmpassword}
+          placeholder="Password"
+        />
+        <Button title="Register" onPress={handleSignup} />
+        <Button
+          title="Already Registered? Login"
+          onPress={() => navigation.replace("Login")}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.BASE[0],
+  },
   authContent: {
     padding: 16,
     flex: 1,
